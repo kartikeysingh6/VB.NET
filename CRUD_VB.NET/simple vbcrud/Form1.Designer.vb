1073c661bee341e58ed2fe529a1e836d3add0e65
Kartikey Singh <59006007+kartikeysingh6@users.noreply.github.com>
2024-01-16 20:13:08 +0530

    Add files via upload

---
+++ CRUD_VB.NET/simple vbcrud/Form1.Designer.vb
@@ -0,0 +1,299 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.txtsearch = New System.Windows.Forms.TextBox()
+        Me.btnrefresh = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.btnclear = New System.Windows.Forms.Button()
+        Me.btnsave = New System.Windows.Forms.Button()
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtname = New System.Windows.Forms.TextBox()
+        Me.txtaddress = New System.Windows.Forms.TextBox()
+        Me.cmbjoin = New System.Windows.Forms.ComboBox()
+        Me.Panel4 = New System.Windows.Forms.Panel()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.btnupdate = New System.Windows.Forms.Button()
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.EditToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.DeleteToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Panel1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        Me.Panel4.SuspendLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.ContextMenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Panel1.Controls.Add(Me.txtsearch)
+        Me.Panel1.Controls.Add(Me.btnrefresh)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(552, 48)
+        Me.Panel1.TabIndex = 0
+        '
+        'txtsearch
+        '
+        Me.txtsearch.Location = New System.Drawing.Point(307, 15)
+        Me.txtsearch.Name = "txtsearch"
+        Me.txtsearch.Size = New System.Drawing.Size(72, 20)
+        Me.txtsearch.TabIndex = 4
+        Me.txtsearch.Visible = False
+        '
+        'btnrefresh
+        '
+        Me.btnrefresh.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnrefresh.Location = New System.Drawing.Point(393, 10)
+        Me.btnrefresh.Name = "btnrefresh"
+        Me.btnrefresh.Size = New System.Drawing.Size(97, 28)
+        Me.btnrefresh.TabIndex = 0
+        Me.btnrefresh.Text = "Refresh"
+        Me.btnrefresh.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(10, 7)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(266, 20)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Vb.Net and Mysql CRUD Project"
+        '
+        'Panel2
+        '
+        Me.Panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Panel2.Controls.Add(Me.btnclear)
+        Me.Panel2.Controls.Add(Me.btnsave)
+        Me.Panel2.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.Panel2.Location = New System.Drawing.Point(0, 336)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(552, 47)
+        Me.Panel2.TabIndex = 1
+        '
+        'btnclear
+        '
+        Me.btnclear.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnclear.Location = New System.Drawing.Point(448, 5)
+        Me.btnclear.Name = "btnclear"
+        Me.btnclear.Size = New System.Drawing.Size(97, 28)
+        Me.btnclear.TabIndex = 0
+        Me.btnclear.Text = "Clear"
+        Me.btnclear.UseVisualStyleBackColor = True
+        '
+        'btnsave
+        '
+        Me.btnsave.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnsave.Location = New System.Drawing.Point(345, 5)
+        Me.btnsave.Name = "btnsave"
+        Me.btnsave.Size = New System.Drawing.Size(97, 28)
+        Me.btnsave.TabIndex = 0
+        Me.btnsave.Text = "Save"
+        Me.btnsave.UseVisualStyleBackColor = True
+        '
+        'Panel3
+        '
+        Me.Panel3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Panel3.Dock = System.Windows.Forms.DockStyle.Right
+        Me.Panel3.Location = New System.Drawing.Point(534, 48)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(18, 288)
+        Me.Panel3.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(12, 54)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(60, 20)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Name:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(12, 91)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(80, 20)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Address:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(12, 139)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(71, 20)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Join Date:"
+          '
+        'Label5
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(12, 139)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(71, 20)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Salary:"
+        '
+        'txtname
+        '
+        Me.txtname.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtname.Location = New System.Drawing.Point(106, 54)
+        Me.txtname.Name = "txtname"
+        Me.txtname.Size = New System.Drawing.Size(197, 26)
+        Me.txtname.TabIndex = 4
+        '
+        'txtaddress
+        '
+        Me.txtaddress.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtaddress.Location = New System.Drawing.Point(106, 88)
+        Me.txtaddress.Multiline = True
+        Me.txtaddress.Name = "txtaddress"
+        Me.txtaddress.Size = New System.Drawing.Size(197, 45)
+        Me.txtaddress.TabIndex = 4
+        '
+        'cmbjoin
+        '
+        Me.cmbjoin.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmbjoin.Location = New System.Drawing.Point(106, 141)
+        Me.cmbjoin.Multiline = True
+        Me.cmbjoin.Name = "cmbjoin"
+        Me.cmbjoin.Size = New System.Drawing.Size(197, 28)
+        Me.cmbjoin.TabIndex = 5
+        '
+        'Panel4
+        '
+        Me.Panel4.Controls.Add(Me.DataGridView1)
+        Me.Panel4.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.Panel4.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Panel4.Location = New System.Drawing.Point(0, 175)
+        Me.Panel4.Name = "Panel4"
+        Me.Panel4.Size = New System.Drawing.Size(534, 161)
+        Me.Panel4.TabIndex = 6
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.DataGridView1.Location = New System.Drawing.Point(0, 0)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
+        Me.DataGridView1.Size = New System.Drawing.Size(534, 161)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'btnupdate
+        '
+        Me.btnupdate.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnupdate.Location = New System.Drawing.Point(381, 80)
+        Me.btnupdate.Name = "btnupdate"
+        Me.btnupdate.Size = New System.Drawing.Size(110, 52)
+        Me.btnupdate.TabIndex = 7
+        Me.btnupdate.Text = "Update"
+        Me.btnupdate.UseVisualStyleBackColor = True
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.Font = New System.Drawing.Font("Segoe UI Emoji", 10.0!, System.Drawing.FontStyle.Bold)
+        Me.ContextMenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.EditToolStripMenuItem, Me.DeleteToolStripMenuItem})
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(124, 52)
+        '
+        'EditToolStripMenuItem
+        '
+        Me.EditToolStripMenuItem.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold)
+        Me.EditToolStripMenuItem.Name = "EditToolStripMenuItem"
+        Me.EditToolStripMenuItem.Size = New System.Drawing.Size(123, 24)
+        Me.EditToolStripMenuItem.Text = "Edit"
+        '
+        'DeleteToolStripMenuItem
+        '
+        Me.DeleteToolStripMenuItem.Name = "DeleteToolStripMenuItem"
+        Me.DeleteToolStripMenuItem.Size = New System.Drawing.Size(123, 24)
+        Me.DeleteToolStripMenuItem.Text = "Delete"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(552, 383)
+        Me.Controls.Add(Me.btnupdate)
+        Me.Controls.Add(Me.Panel4)
+        Me.Controls.Add(Me.cmbjoin)
+        Me.Controls.Add(Me.txtaddress)
+        Me.Controls.Add(Me.txtname)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Panel3)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel4.ResumeLayout(False)
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ContextMenuStrip1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents btnrefresh As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents btnclear As Button
+    Friend WithEvents btnsave As Button
+    Friend WithEvents Panel3 As Panel
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtname As TextBox
+    Friend WithEvents txtaddress As TextBox
+    Friend WithEvents cmbjoin As ComboBox
+    Friend WithEvents Panel4 As Panel
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents btnupdate As Button
+    Friend WithEvents ContextMenuStrip1 As ContextMenuStrip
+    Friend WithEvents EditToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents txtsearch As TextBox
+    Friend WithEvents ToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents DeleteToolStripMenuItem As ToolStripMenuItem
+End Class
---
+++ CRUD_VB.NET/simple vbcrud/Form1.vb
@@ -0,0 +1,137 @@
+﻿Imports MySql.Data.MySqlClient
+
+Public Class Form1
+    Dim myconnection As String = "server=localhost;user id=root;password=mysql123;database=db_vbcrud"
+    Dim conn As New MySqlConnection(myconnection)
+    Dim cmd As New MySqlCommand
+    Dim sql As String
+    Private rowindex As Integer
+    Dim da As New MySqlDataAdapter
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Try
+            Dim query As String = "Select id as'Std.ID',name as 'Student Name',address as'Address',join as 'join' from tbl_employee where status=0"
+            Dim adpt As New MySqlDataAdapter(query, conn)
+            Dim ds As New DataSet()
+            adpt.Fill(ds, "Std")
+            DataGridView1.DataSource = ds.Tables(0)
+            conn.Close()
+
+
+
+        Catch ex As Exception
+            MessageBox.Show(ex.ToString())
+            Exit Sub
+        End Try
+
+    End Sub
+    Sub clear()
+        txtname.Text = ""
+        txtaddress.Text = ""
+        cmbjoin.SelectedIndex = -1
+        txtsearch.Text = ""
+
+    End Sub
+
+    Private Sub btnclear_Click(sender As Object, e As EventArgs) Handles btnclear.Click
+        clear()
+
+    End Sub
+
+    Private Sub btnrefresh_Click(sender As Object, e As EventArgs) Handles btnrefresh.Click
+        Form1_Load(sender, e)
+    End Sub
+
+    Private Sub DataGridView1_CellMouseUp(sender As Object, e As DataGridViewCellMouseEventArgs) Handles DataGridView1.CellMouseUp
+        If DataGridView1.RowCount <= 0 Then
+            Exit Sub
+        End If
+        Try
+            If e.Button = MouseButtons.Right Then
+                Me.DataGridView1.Rows(e.RowIndex).Selected = True
+                Me.rowindex = e.RowIndex
+                Me.DataGridView1.CurrentCell = Me.DataGridView1.Rows(e.RowIndex).Cells(1)
+                Me.ContextMenuStrip1.Show(Me.DataGridView1, e.Location)
+                ContextMenuStrip1.Show(Cursor.Position)
+            End If
+        Catch ex As Exception
+            Exit Sub
+        End Try
+    End Sub
+
+    Private Sub EditToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles EditToolStripMenuItem.Click
+        txtsearch.Text = DataGridView1.CurrentRow.Cells(0).Value.ToString()
+
+    End Sub
+
+    Private Sub txtsearch_TextChanged(sender As Object, e As EventArgs) Handles txtsearch.TextChanged
+        If txtsearch.Text = "" Then
+            Exit Sub
+        End If
+        Try
+            sql = "select * from tbl_employee where id=" & Val(txtsearch.Text)
+            conn.ConnectionString = myconnection
+            conn.Open()
+            With cmd
+                .Connection = conn
+                .CommandText = sql
+            End With
+            Dim publictable As New DataTable
+            da.SelectCommand = cmd
+            da.Fill(publictable)
+            txtname.Text = publictable.Rows(0).Item(1)
+            txtaddress.Text = publictable.Rows(0).Item(2)
+            cmbjoin.Text = publictable.Rows(0).Item(3)
+        Catch ex As Exception
+            MessageBox.Show(ex.ToString())
+        Finally
+            da.Dispose()
+            conn.Close()
+
+        End Try
+    End Sub
+
+    Private Sub btnupdate_Click(sender As Object, e As EventArgs) Handles btnupdate.Click
+        Try
+            conn.ConnectionString = myconnection
+            conn.Open()
+            sql = "Update tbl_employee set name=@name,address=@address,join=@join where id=" & Val(txtsearch.Text)
+            cmd.Connection = conn
+            cmd.CommandText = sql
+            cmd.Parameters.AddWithValue("@name", txtname.Text)
+            cmd.Parameters.AddWithValue("@address", txtaddress.Text)
+            cmd.Parameters.AddWithValue("@join", cmbjoin.Text)
+            Dim r As Integer
+            r = cmd.ExecuteNonQuery
+            If r > 0 Then
+                MsgBox("Join Record Updated....!")
+                clear()
+                conn.Close()
+                cmd.Parameters.Clear()
+
+            End If
+        Catch ex As Exception
+            Exit Sub
+        End Try
+    End Sub
+
+    Private Sub DeleteToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles DeleteToolStripMenuItem.Click
+        Try
+            conn.ConnectionString = myconnection
+            conn.Open()
+            sql = "Update tbl_employee set status=1 where id=" & Val(DataGridView1.CurrentRow.Cells(0).Value.ToString())
+            cmd.Connection = conn
+            cmd.CommandText = sql
+            Dim r As Integer
+            r = cmd.ExecuteNonQuery
+            If r > 0 Then
+                MsgBox("Join Record Deleted....!")
+                clear()
+                conn.Close()
+            End If
+        Catch ex As Exception
+            Exit Sub
+        End Try
+    End Sub
+End Class
